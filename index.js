da2e9b32a5129fb48e1689b45d1ede60f83ce726
mariee444 <79097315+mariee444@users.noreply.github.com>
2022-02-07 16:36:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,54 @@ const server = require('http').createServer(app);
 const port = 3000;
 
 const axios = require('axios');
+// Abgerufen werden können Infos über:
+// https://pokeapi.co/api/v2/pokemon/name
 const pokeapi = 'https://pokeapi.co/api/v2/';
+
+const htmlHead = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hallo Welt!</title>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    `;
+const htmlFoot = `
+  </body>
+</html>`;
 
 server.listen(port, () => {
   console.log('Webserver läuft. Port: %d', port);
 });
 
-app.get('/', (req, res) => {
+app.get('/:name*', (req, res) => {
   //console.log(req);
   // res.send('Hällo ägän');
   axios
-    .get(pokeapi + 'pokemon/ditto')
-    .then((reponse) => {
+    .get(pokeapi + 'pokemon/' + req.params['name'])
+    .then((response) => {
       res.send(
-        '<img src="' +
-          reponse.data.sprites.other['official-artwork'].front_default +
-          '">'
+        htmlHead +
+          '<h1>Hier ist ' +
+          req.params['name'] +
+          '</h1><img src="' +
+          response.data.sprites.other['official-artwork'].front_default +
+          '"><p>' +
+          req.params['name'] +
+          ' ist das ' +
+          response.data.order +
+          'ste Pokemon.</p>' +
+          '<p>' +
+          req.params['name'] +
+          ' hat das Gewicht ' +
+          response.data.weight +
+          '.</p>' +
+          '<p>' +
+          req.params['name'] +
+          ' hat die Größe ' +
+          response.data.height +
+          '.</p>' +
+          htmlFoot
       );
     })
     .catch((error) => {
