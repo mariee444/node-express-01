1cf2e9c9a8e410c308879266764ed59207807088
mariee444 <79097315+mariee444@users.noreply.github.com>
2022-02-07 15:42:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const port = 3000;
-server.listen(port, function () {
-  console.log('Webserver läuft fort: %d', port);
+
+const axios = require('axios');
+const pokeapi = 'https://pokeapi.co/api/v2/';
+
+server.listen(port, () => {
+  console.log('Webserver läuft. Port: %d', port);
 });
+
+app.get('/', (req, res) => {
+  //console.log(req);
+  // res.send('Hällo ägän');
+  axios
+    .get(pokeapi + 'pokemon/ditto')
+    .then((reponse) => {
+      res.send(
+        '<img src="' +
+          reponse.data.sprites.other['official-artwork'].front_default +
+          '">'
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send(error);
+    });
+});
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+
